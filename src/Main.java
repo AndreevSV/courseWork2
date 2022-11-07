4a21c019776877baf3ca78932ee58c4d54620e53
AndreevSV <51971450+AndreevSV@users.noreply.github.com>
2022-11-07 23:15:30 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        String[] array = new String[] {"Рустам", "Рустам", "Владимир", "Владимир", "Кирилл", "Андрей", "Кирилл"};
+        System.out.println(Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+            String arg = array[i];
+            if  (array[i] != null) {
+                for (int j = i+1; j < array.length; j++) {
+                    if (arg.equals(array[j])) {
+                        array[j] = null;
+                    }
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]  == null) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] != null) {
+                        array[i] = array[j];
+                        array[j] = null;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 
 }
